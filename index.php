2c2fbeea29974558a7815371842622519cac7e42
partha Sarothi <parthasarothi@ablion.in>
2018-08-28 11:54:02 +0530

    Resolved merge conflict by keeping README.md file.

--- index.php
+++ index.php
@@ -1,3 +1,100 @@
+<html>
+
+
+
+<body>
+<script type="text/javascript">
+
+$(document).ready(function() {
+<<<<<<< index.php
+<<<<<<< index.php
+    console.log( "ready!" )
+
+=======
+    console.log( "ready!" );aaa
+     console.log( "two!" );aaa    
+    
+>>>>>>> ace.php
+=======
+    console.log( "ready!" );aaa
+     console.log( "two!" );aaa    
+    
+>>>>>>> ace.php
+})
+
+</script>
+
+</body>
+
+<div class="test">
+
+
+</div>
+
+<?php
+
+echo"new line";
+
+
+?>
+
+
+</html>
+
+
+<<<<<<< index.php
+=======
+
+
+
+
+
+
+
+
+
+<abbr>
+
+New Test
+</abbr>
+
+
+
+<address>
+wow
+
+</address>
+
+
+<bdi>
+
+New word
+</bdi>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<p>Text structure</p>
+
+
+
+<s>
+Manage text
+
+</s>
+
+
+</html>
+>>>>>>> ace.php
 <?php
 
 echo"here";
